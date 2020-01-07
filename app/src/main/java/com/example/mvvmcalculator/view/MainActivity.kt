73d7466effb309f72cce938f57da791d278aa40c
victory316 <csh153@gmail.com>
2020-01-08 01:37:01 +0900

    tested basic functions

--- app/src/main/java/com/example/mvvmcalculator/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmcalculator/view/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.mvvmcalculator.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.mvvmcalculator.R
+import com.example.mvvmcalculator.databinding.ActivityMainBinding
+import com.example.mvvmcalculator.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding : ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = MainViewModel()
+
+        mainBinding.viewModel = viewModel
     }
 }
--- app/src/main/java/com/example/mvvmcalculator/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmcalculator/viewmodel/MainViewModel.kt
@@ -1,11 +1,56 @@
 package com.example.mvvmcalculator.viewmodel
 
+import android.util.Log
 import androidx.databinding.BaseObservable
+import androidx.databinding.ObservableField
 
 class MainViewModel: BaseObservable() {
+    var currentInput = ObservableField<String> ()
+    var tempInput = ObservableField<String> ()
 
-    fun doAdd() {}
-    fun doMinus() {}
-    fun doMultiply() {}
-    fun doAC() {}
+    var first = 0
+    var second = 0
+    var result = 0
+    var operator = 0
+    var stringBuffer = StringBuffer()
+
+    fun inputNumber(number: Int) {
+        stringBuffer.append(number)
+        currentInput.set(stringBuffer.toString())
+        Log.d("inputTest", "number : $number")
+    }
+
+    fun doAC() {
+        stringBuffer.delete(0, stringBuffer.capacity())
+        currentInput.set("")
+        operator = 0
+    }
+
+    fun doOperation(operation: Int) {
+        tempInput.set(stringBuffer.toString())
+        stringBuffer.delete(0, stringBuffer.capacity())
+        currentInput.set("")
+        operator = operation
+    }
+
+    fun getTheResult() {
+        if (operator != 0) {
+            first = tempInput.get()!!.toInt()
+            second = currentInput.get()!!.toInt()
+
+            when (operator) {
+                PLUS -> currentInput.set(first.plus(second).toString())
+                MINUS -> currentInput.set(first.minus(second).toString())
+                MULTIPLY -> currentInput.set(first.times(second).toString())
+                DIVISION -> currentInput.set(first.div(second).toString())
+            }
+        }
+    }
+
+    companion object {
+        const val PLUS = 1
+        const val MINUS = 2
+        const val MULTIPLY = 3
+        const val DIVISION = 4
+    }
 }
