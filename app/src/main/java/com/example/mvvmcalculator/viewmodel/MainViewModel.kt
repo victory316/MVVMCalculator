215329b5d75c61dbaa6f662eee0080d167c29cb7
victory316 <csh153@gmail.com>
2020-01-08 11:40:49 +0900

    finishing applying all functions that intented to

--- app/src/main/java/com/example/mvvmcalculator/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmcalculator/viewmodel/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.mvvmcalculator.viewmodel
 
-import android.util.Log
 import androidx.databinding.BaseObservable
 import androidx.databinding.ObservableField
 
@@ -14,9 +13,12 @@ class MainViewModel: BaseObservable() {
     private var second = 0.0
     private var operator = 0
     private var stringBuffer = StringBuffer()
+    private var newOperation = false
 
     fun inputNumber(number: Int) {
-        if (number == -1) {
+
+        // input이 COMMA일때와 number일때의 처리
+        if (number == COMMA) {
             stringBuffer.append(".")
         } else {
             stringBuffer.append(number)
@@ -25,34 +27,47 @@ class MainViewModel: BaseObservable() {
     }
 
     fun doAC() {
+
+        // AC 입력시 연산에 관한 데이터 초기화
         stringBuffer.delete(0, stringBuffer.capacity())
         currentInput.set("")
+        tempInput.set("")
         latestResult.set("")
         operator = 0
     }
 
     fun doOperation(operation: Int) {
 
+        // 입력받은 operation에 따라 연산 이전에 필요한 데이터 설정
         latestInput.set(stringBuffer.toString())
         tempInput.set(stringBuffer.toString())
         stringBuffer.delete(0, stringBuffer.capacity())
         currentInput.set("")
         operator = operation
+        newOperation = true
     }
 
     fun getTheResult() {
-        Log.d("result", "before temp : ${tempInput.get().toString()} | current : ${currentInput.get().toString()}")
 
         // 우선적으로 operator와 입력값이 존재하는지 확인
         if (operator != 0 && !currentInput.get().isNullOrBlank()) {
 
-            Log.d("result", "data : ${latestResult.get()}")
-
             // 가장 최근 result가 저장되어있는지 확인, 존재 유무에 따라 first 및 second value 설정
             if (!latestResult.get().isNullOrBlank()) {
-                first = latestResult.get()!!.toDouble()
-                second = tempInput.get()!!.toDouble()
+
+                if (!newOperation) {
+                    // case 1 : 신규연산 없이 결과 버튼을 눌렀을 때
+                    first = latestResult.get()!!.toDouble()
+                    second = tempInput.get()!!.toDouble()
+                } else {
+                    // case 2때 : AC 이전 신규연산을 수행했을때 때
+                    first = latestResult.get()!!.toDouble()
+                    second = currentInput.get()!!.toDouble()
+                }
+
             } else {
+
+                // case 3 : 최근 결과값 없이 AC 수행 혹은 초기에 결과 버튼을 눌렀을 때
                 first = latestInput.get()!!.toDouble()
                 second = currentInput.get()!!.toDouble()
             }
@@ -67,7 +82,7 @@ class MainViewModel: BaseObservable() {
             latestResult.set(currentInput.get().toString())
         }
 
-        Log.d("result", "after temp : ${tempInput.get().toString()} | current : ${currentInput.get().toString()} | latest :  ${latestResult.get().toString()}")
+        newOperation = false
     }
 
     companion object {
@@ -75,5 +90,6 @@ class MainViewModel: BaseObservable() {
         const val MINUS = 2
         const val MULTIPLY = 3
         const val DIVISION = 4
+        const val COMMA = -1
     }
 }
