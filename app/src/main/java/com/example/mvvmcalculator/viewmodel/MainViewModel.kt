f7cfba2fd803a411d5bad9bf280bb0a5628b57a9
victory316 <csh153@gmail.com>
2020-01-08 11:16:45 +0900

    testing adding dot, trying to do continuous calculation

--- app/src/main/java/com/example/mvvmcalculator/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmcalculator/viewmodel/MainViewModel.kt
@@ -7,26 +7,33 @@ import androidx.databinding.ObservableField
 class MainViewModel: BaseObservable() {
     var currentInput = ObservableField<String> ()
     var tempInput = ObservableField<String> ()
+    var latestInput = ObservableField<String> ()
+    var latestResult = ObservableField<String> ()
 
-    var first = 0
-    var second = 0
-    var result = 0
-    var operator = 0
-    var stringBuffer = StringBuffer()
+    private var first = 0.0
+    private var second = 0.0
+    private var operator = 0
+    private var stringBuffer = StringBuffer()
 
     fun inputNumber(number: Int) {
-        stringBuffer.append(number)
+        if (number == -1) {
+            stringBuffer.append(".")
+        } else {
+            stringBuffer.append(number)
+        }
         currentInput.set(stringBuffer.toString())
-        Log.d("inputTest", "number : $number")
     }
 
     fun doAC() {
         stringBuffer.delete(0, stringBuffer.capacity())
         currentInput.set("")
+        latestResult.set("")
         operator = 0
     }
 
     fun doOperation(operation: Int) {
+
+        latestInput.set(stringBuffer.toString())
         tempInput.set(stringBuffer.toString())
         stringBuffer.delete(0, stringBuffer.capacity())
         currentInput.set("")
@@ -34,9 +41,21 @@ class MainViewModel: BaseObservable() {
     }
 
     fun getTheResult() {
-        if (operator != 0) {
-            first = tempInput.get()!!.toInt()
-            second = currentInput.get()!!.toInt()
+        Log.d("result", "before temp : ${tempInput.get().toString()} | current : ${currentInput.get().toString()}")
+
+        // 우선적으로 operator와 입력값이 존재하는지 확인
+        if (operator != 0 && !currentInput.get().isNullOrBlank()) {
+
+            Log.d("result", "data : ${latestResult.get()}")
+
+            // 가장 최근 result가 저장되어있는지 확인, 존재 유무에 따라 first 및 second value 설정
+            if (!latestResult.get().isNullOrBlank()) {
+                first = latestResult.get()!!.toDouble()
+                second = tempInput.get()!!.toDouble()
+            } else {
+                first = latestInput.get()!!.toDouble()
+                second = currentInput.get()!!.toDouble()
+            }
 
             when (operator) {
                 PLUS -> currentInput.set(first.plus(second).toString())
@@ -44,7 +63,11 @@ class MainViewModel: BaseObservable() {
                 MULTIPLY -> currentInput.set(first.times(second).toString())
                 DIVISION -> currentInput.set(first.div(second).toString())
             }
+
+            latestResult.set(currentInput.get().toString())
         }
+
+        Log.d("result", "after temp : ${tempInput.get().toString()} | current : ${currentInput.get().toString()} | latest :  ${latestResult.get().toString()}")
     }
 
     companion object {
